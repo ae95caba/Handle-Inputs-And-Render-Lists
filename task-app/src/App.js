20a7a92cd2c7f318f25ec335e7bff143e3c74115
Andre Espinoza <ae95caba@gmail.com>
2023-02-04 05:05:11 -0300

    working but with id and class problems

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,15 +1,81 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import { ListItem } from "./components/Overview";
+import { Overview } from "./components/Overview";
+import uniqid from "uniqid";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { tasks: [] };
+  }
+
+  addTask = (e) => {
+    this.setState((prevState) => ({
+      tasks: [
+        ...this.state.tasks,
+        { tittle: document.getElementById("input").value, id: uniqid() },
+      ],
+    }));
+  };
+
+  preventDefault(e) {
+    console.log("submit prevented");
+    e.preventDefault();
+    document.getElementById("input").value = "";
+  }
+
+  deleteItem = (e) => {
+    let arr = this.state.tasks;
+    const index = arr.indexOf(e.target.id);
+    arr.splice(index, 1);
+    this.setState({ tasks: arr });
+  };
+
+  editItem = (e) => {
+    document.getElementById(`${e.target.className}-span`).style.display =
+      "none";
+    document.getElementById(`${e.target.className}-input`).style.display =
+      "inline";
+    document.getElementById(
+      `${e.target.className}-confirm-button`
+    ).style.display = "inline";
+    e.target.style.display = "none";
+  };
+
+  confirmEdit = (e) => {
+    document.getElementById(`${e.target.className}-span`).style.display =
+      "inline";
+    document.getElementById(`${e.target.className}-input`).style.display =
+      "none";
+    document.getElementById(`${e.target.className}-edit-button`).style.display =
+      "inline";
+    e.target.style.display = "none";
+    /////
+    const index = this.state.tasks.indexOf(e.target.className);
+    const arr = this.state.tasks;
+    arr[index] = document.getElementById(`${e.target.className}-input`).value;
+
+    this.setState({ tasks: arr });
+  };
+
   render() {
+    console.log(this.state.tasks);
+
     return (
       <div className="App">
-        <input />
-        <button>Botton</button>
-        <ListItem />
+        <form action="" onSubmit={this.preventDefault}>
+          <label>Tarea</label>
+          <input id="input" />
+          <button onClick={this.addTask}>Add</button>
+        </form>
+
+        <Overview
+          arr={this.state.tasks}
+          deleteItem={this.deleteItem}
+          editItem={this.editItem}
+          confirmEdit={this.confirmEdit}
+        />
       </div>
     );
   }
--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -1,7 +1,37 @@
 import React from "react";
 
-export class ListItem extends React.Component {
+export class Overview extends React.Component {
   render() {
-    return <li>Hola ameo</li>;
+    return (
+      <ul>
+        {this.props.arr.map((value) => {
+          return (
+            <li key={value}>
+              {this.props.arr.indexOf(value) + 1}-
+              <span id={`${value}-span`}>{value}</span>{" "}
+              <input style={{ display: "none" }} id={`${value}-input`} />
+              <button id={value} onClick={this.props.deleteItem}>
+                Delete
+              </button>
+              <button
+                onClick={this.props.editItem}
+                className={value}
+                id={`${value}-edit-button`}
+              >
+                Edit
+              </button>
+              <button
+                className={value}
+                onClick={this.props.confirmEdit}
+                style={{ display: "none" }}
+                id={`${value}-confirm-button`}
+              >
+                Confirm
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
   }
 }
